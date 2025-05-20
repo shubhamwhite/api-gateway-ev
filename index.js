bb0801e34d7f54e94f86545ecada294152305084
Whitelotuscorp <contact@whitelotuscorporation.com>
2025-05-20 10:48:31 +0530

    testing moe

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-
 const gateway = require('fast-gateway')
 const color = require('./helper/color.helper')
+const config = require('./config')
+const port = config.get('PORT') || 5000
 
-const port = 4000
 const server = gateway({
   routes: [
     {
       prefix: '/api/v1/company',
       target: 'http://localhost:3005/',
       hooks: {
-        onRequest (req, res) {
-          console.log(`Request received: ${req.method} ${req.url}`)
+        onRequest(req, res) {
+          color.bold(`Request received: ${req.method} ${req.url}`)
         }
       }
     },
@@ -18,27 +18,24 @@ const server = gateway({
       prefix: '/api/v1/auth',
       target: 'http://localhost:3004/',
       hooks: {
-        onRequest (req, res) {
-          console.log(`Request received: ${req.method} ${req.url}`)
+        onRequest(req, res) {
+          color.bold(`Request received: ${req.method} ${req.url}`)
         }
       }
     }
   ]
 })
 
-server.get('/gateway',(req,res) => {
+server.get('/gateway', (req, res) => {
   res.send('gateway is running')
 })
 
 ;(async () => {
   try {
-    // Optional: DB connection (if needed)
-    // await connectDB();
-
     await server.start(port)
     color.success(`api-gateway server is running on ${port} number`)
   } catch (err) {
-    color.error('Error starting api-gateway server:', err)
+    color.error('Error starting api-gateway server :', err)
     process.exit(1)
   }
 })()
